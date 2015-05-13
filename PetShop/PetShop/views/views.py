2a8bec14526e75ec7d5eaf1ba90d85bcf5816803
danstonebuilt <dnansel@hotmail.com>
2015-05-13 15:36:46 -0300

    1

---
+++ PetShop/PetShop/views/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'daniel.anselmo'
+
+import os
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+def show_homepage(request):
+    return render(request, 'base.html')
+
+def show(request):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return HttpResponse(os.path.join(BASE_DIR, "templates").replace('\\', '/'))
