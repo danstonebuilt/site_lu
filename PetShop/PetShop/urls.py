cce1549ab70889f6cdb342555de79b03af345336
danstonebuilt <dnansel@hotmail.com>
2015-05-13 17:19:18 -0300

    2

--- PetShop/PetShop/urls.py
+++ PetShop/PetShop/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from PetShop.views.views import show_homepage, show
+from PetShop.views.views import show_homepage, show, homepage
 
 urlpatterns = [
     url(r'^tst/', show_homepage),
+    url(r'^2/', homepage),
     url(r'^1/', show),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- PetShop/PetShop/views/views.py
+++ PetShop/PetShop/views/views.py
@@ -10,5 +10,10 @@ def show_homepage(request):
     return render(request, 'base.html')
 
 def show(request):
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return HttpResponse(os.path.join(BASE_DIR, "templates").replace('\\', '/'))
+    #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #return HttpResponse(os.path.join(BASE_DIR, "templates").replace('\\', '/'))
+    BASE_DIR = os.path.join(os.path.dirname(__file__), 'static', )
+    return HttpResponse(BASE_DIR)
+
+def homepage(request):
+    return render(request, 'home.html')
